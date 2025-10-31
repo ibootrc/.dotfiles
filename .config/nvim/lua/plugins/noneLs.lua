de1a898e1ee05cec249a4784399907b651421c3b
Latif <ibootrc@gmail.com>
2025-10-31 14:57:09 +0100

    fix: update nonels.lua

--- .config/nvim/lua/plugins/noneLs.lua
+++ .config/nvim/lua/plugins/noneLs.lua
@@ -1,28 +1,42 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local none_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require "null-ls"
 
-		none_ls.setup({
-			sources = {
-				none_ls.builtins.formatting.prettier,  -- JavaScript/TypeScript
-				none_ls.builtins.formatting.stylua,    -- Lua
-				none_ls.builtins.formatting.black,     -- Python
-				none_ls.builtins.formatting.shfmt,     -- Shell scripts
-			},
-		})
+    -- Define formatting sources
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.prettier, -- JS/TS
+        null_ls.builtins.formatting.stylua, -- Lua
+        null_ls.builtins.formatting.black, -- Python
+        null_ls.builtins.formatting.shfmt, -- Shell
+      },
+      -- Attach per buffer
+      on_attach = function(client, bufnr)
+        if client.supports_method "textDocument/formatting" then
+          -- Create a group to prevent duplicate autocmds
+          local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
 
-		-- Auto-format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function()
-				vim.lsp.buf.format({
-					async = false,
-					filter = function(client)
-						return client.supports_method("textDocument/formatting")
-					end,
-				})
-			end,
-		})
-	end,
+          -- Clear existing autocmds for this buffer in the group
+          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+
+          -- Auto-format on save
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format {
+                bufnr = bufnr,
+                filter = function(format_client)
+                  -- Only use null-ls for formatting
+                  return format_client.name == "null-ls"
+                end,
+                async = false,
+              }
+            end,
+          })
+        end
+      end,
+    }
+  end,
 }
