6db5856adede2ed9b5de6f9c227a8f3879df59e4
Latif <ibootrc@gmail.com>
2025-10-31 23:28:50 +0100

    fix: update noice.lua

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,45 +1,46 @@
 return {
-	-- Load nvim-notify before Noice
-	{
-		"rcarriga/nvim-notify",
-		config = function()
-			require("notify").setup({
-				render = "compact", -- Compact style
-				stages = "fade_in_slide_out", -- Animation
-				timeout = 3000,
-				background_colour = "#1e1e1e",
-			})
-			vim.notify = require("notify") -- Set as default notify function
-		end,
-	},
+  -- Load nvim-notify before Noice
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      ---@diagnostic disable-next-line: undefined-field
+      require("notify").setup {
+        render = "compact", -- Compact style
+        stages = "fade_in_slide_out", -- Animation
+        timeout = 3000,
+        background_colour = "#1e1e1e",
+      }
+      vim.notify = require "notify" -- Set as default notify function
+    end,
+  },
 
-	-- load Noice
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-		opts = {
-			views = {
-				cmdline_popup = {
-					position = { row = "50%", col = "50%" },
-					size = { width = "90%", height = "auto" },
-				},
-			},
-			presets = {
-				command_palette = true,
-				bottom_search = true,
-			},
-			notify = {
-				enabled = true, -- Ensure Noice notifications are active
-			},
-		},
-		config = function()
-			require("noice").setup()
-		end,
-	},
-	-- Dismiss Noice Message
-	vim.keymap.set("n", "<leader>mm", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" }),
+  -- load Noice
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      views = {
+        cmdline_popup = {
+          position = { row = "50%", col = "50%" },
+          size = { width = "90%", height = "auto" },
+        },
+      },
+      presets = {
+        command_palette = true,
+        bottom_search = true,
+      },
+      notify = {
+        enabled = true, -- Ensure Noice notifications are active
+      },
+    },
+    config = function()
+      require("noice").setup()
+    end,
+  },
+  -- Dismiss Noice Message
+  vim.keymap.set("n", "<leader>mm", "<cmd>NoiceDismiss<CR>", { desc = "Dismiss Noice Message" }),
 }
