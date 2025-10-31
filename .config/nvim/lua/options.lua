a64a71ded16fffccb5e0664229406a9c2f302bb3
Latif <ibootrc@gmail.com>
2025-10-31 12:51:38 +0100

    fix: update LSP config for Neovim 0.11+ compatibility

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -18,33 +18,32 @@ vim.opt.list = true
 vim.opt.breakindent = false
 vim.opt.signcolumn = "yes"
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.wildignore:append { "*/node_modules/**" }
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.keymap.set("n", "s", "<Nop>", { noremap = true, silent = true })
 
 -- Highlight settingsvim.cmd([[highlight LineNr guibg=bg]])
-vim.cmd([[
+vim.cmd [[
 augroup CustomCursorLineHighlight
 autocmd!
 autocmd VimEnter * highlight CursorLine guifg=#A8D8E6 guibg=#4a4a4a
-augroup END
-]])
+augroup ENDD
+]]
 
 -- UI and Visual Customizations
 -- Customize the appearance of fold column, vertical splits, and fill characters
 vim.opt.foldcolumn = "0"
-vim.cmd([[highlight FoldColumn guibg=bg]])
-vim.cmd([[highlight VertSplit guifg=bg guibg=bg]])
+vim.cmd [[highlight FoldColumn guibg=bbg]]
+vim.cmd [[highlight VertSplit guifg=bg guibg=bbg]]
 vim.opt.fillchars = {
-	vert = "|", -- Simple vertical line
-	fold = "-", -- Simple fold separator
-	eob = "~", -- Keep classic end-of-buffer markers
-	diff = "-", -- Horizontal line for diffs
+  vert = "|", -- Simple vertical line
+  fold = "-", -- Simple fold separator
+  eob = " ",  -- Fix: must be a single space, not empty
+  diff = "-", -- Horizontal line for diffs
 }
-
 -- Key mappings
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -56,8 +55,8 @@ vim.keymap.set("n", "<C-p>", "<cmd>lprev<CR>zz")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
-	vim.cmd("so")
-	print("Config Reloaded!")
+	vim.cmd "so"
+	print "Config Reloadeed!"
 end)
 
 -- Use CTRL+<hjkl> to switch between windows
@@ -66,17 +65,16 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- Help pages in vertical splits
+-- Move help windows to a vertical split on the right
 vim.api.nvim_create_autocmd("BufWinEnter", {
-	group = vim.api.nvim_create_augroup("help_window_right", {}),
-	pattern = { "*.txt" },
+	group = vim.api.nvim_create_augroup("HelpWindowRight", {}),
+	pattern = "help",
 	callback = function()
-		if vim.o.filetype == "help" then
-			vim.cmd.wincmd("L")
+		if vim.bo.filetype == "help" then
+			vim.cmd("wincmd L")
 		end
 	end,
 })
-
 -- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -97,17 +95,17 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 	end,
 })
 
+
 -- Adjust number width and sign column for help pages
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "help",
 	callback = function()
-		vim.wo.number = true -- Enable line numbers for help files
-		vim.wo.relativenumber = true -- Optional: disable relative numbers for help files
-		vim.wo.numberwidth = 1 -- Reduce number column width
-		vim.wo.signcolumn = "no" -- Remove the sign column in help files
+		vim.wo.number = true            -- Enable line numbers for help files
+		vim.wo.relativenumber = false   -- Disable relative numbers for help files
+		vim.wo.numberwidth = 2          -- Reduce number column width
+		vim.wo.signcolumn = "no"        -- Remove sign column
 	end,
 })
-
 -- Disable the right-click popup menu
 vim.api.nvim_set_keymap("n", "<RightMouse>", "<NOP>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<RightMouse>", "<NOP>", { noremap = true, silent = true })
--- .config/nvim/lua/plugins/lspConfig.lua
+++ .config/nvim/lua/plugins/lspConfig.lua
@@ -1,50 +1,79 @@
 return {
-	-- Mason: LSP package manager
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    lazy = false,
+    config = function()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	-- Mason-LSPConfig: Bridges Mason with LSPConfig
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true, -- Automatically install missing LSP servers
-		},
-	},
+      mason.setup()
+      mason_lspconfig.setup({
+        ensure_installed = { "lua_ls" },
+        automatic_installation = true,
+      })
 
-	-- Neovim LSPConfig: Configures LSP servers
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local function on_attach(_, bufnr)
+        local opts = { buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+      end
 
-			-- Function to run when an LSP attaches
-			local function on_attach(client, bufnr)
-				local opts = { buffer = bufnr }
+      -- Get list of installed servers
+      local servers = mason_lspconfig.get_installed_servers()
 
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-			end
+      for _, server in ipairs(servers) do
+        local config = {
+          capabilities = capabilities,
+          on_attach = on_attach,
+        }
 
-			-- Fetch installed LSP servers from Mason
-			require("mason-lspconfig").setup_handlers({
-				-- Default handler for all installed servers
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-						on_attach = on_attach,
-					})
-				end,
-			})
-		end,
-	},
+        -- 🩵 Custom settings for Lua
+        if server == "lua_ls" then
+          config.settings = {
+            Lua = {
+              runtime = { version = "LuaJIT" },
+              diagnostics = {
+                globals = { "vim" },
+                disable = { "assign-as-condition" },
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              telemetry = { enable = false },
+            },
+          }
+        end
+
+        -- ✅ Use Neovim 0.11+ native LSP loader
+        local ok, lspconfig_mod = pcall(require, "lspconfig.configs")
+        local lsp = ok and vim.lsp or require("lspconfig")
+
+        -- Define server configuration
+        local server_config = vim.tbl_deep_extend("force", {}, config)
+
+        -- New API: vim.lsp.start + vim.lsp.config
+        if vim.lsp.start and vim.lsp.config then
+          local new_cfg = vim.lsp.config(server, server_config)
+          if new_cfg then
+            vim.lsp.start(new_cfg)
+          end
+        else
+          -- Backward compatibility for older nvim builds
+          local ok2, old_lsp = pcall(require, "lspconfig")
+          if ok2 and old_lsp[server] and old_lsp[server].setup then
+            old_lsp[server].setup(server_config)
+          end
+        end
+      end
+    end,
+  },
 }
