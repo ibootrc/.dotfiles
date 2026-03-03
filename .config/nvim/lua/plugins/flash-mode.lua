0b8007aaf6d5197148df4c9bbfa56ac05e3ec42c
Latif <ibootrc@gmail.com>
2026-03-03 01:06:02 +0100

    fix flash.nvim

--- .config/nvim/lua/plugins/flash-mode.lua
+++ .config/nvim/lua/plugins/flash-mode.lua
@@ -1,50 +1,40 @@
 return {
   {
-    "folke/twilight.nvim",
-    lazy = false,
-    config = function()
-      require("twilight").setup {
-        dimming = {
-          alpha = 0.7,
-          color = { "Normal" },
-        },
-        context = 0,
-        expand = { "function", "method", "table", "if_statement" },
-        treesitter = true,
-      }
-    end,
-  },
-
-  -- Flash.nvim
-  {
     "folke/flash.nvim",
-    lazy = false,
-    dependencies = { "folke/twilight.nvim" },
+    keys = { "s", "S", "r", "rr", "<c-s>" },
     opts = {
       highlight = {
         backdrop = true,
-        matches = "Search",
-        priority = 10,
+        matches = "IncSearch",
+        priority = 5000,
       },
       modes = {
-        char = { enabled = true },
+        char = { enabled = true, jump_labels = false },
         search = { enabled = true },
         treesitter = { enabled = false },
       },
-      search = { incremental = true, mode = "current_line" },
-      jump = { autojump = true },
+      search = {
+        incremental = true,
+        mode = "current_line",
+      },
+      jump = {
+        autojump = false,
+      },
+      max_length = 5000,
     },
     config = function(_, opts)
       local flash = require "flash"
-      local twilight = require "twilight"
-
       flash.setup(opts)
 
-      local function flash_with_spotlight(fn)
-        twilight.enable()
-        fn()
-        twilight.disable()
-      end
+      -- Make Flash labels more readable without changing other highlights
+      vim.api.nvim_set_hl(0, "FlashLabel", {
+        fg = "#ffffff", -- bright color for labels
+        bg = nil, -- keep background as-is
+        bold = true,
+        underline = false,
+        reverse = false,
+        italic = false,
+      })
 
       local mappings = {
         { "s", flash.jump, "Flash" },
@@ -54,10 +44,11 @@ return {
         { "<c-s>", flash.toggle, "Toggle Flash Search" },
       }
 
-      for _, k in ipairs(mappings) do
-        vim.keymap.set({ "n", "x", "o" }, k[1], function()
-          flash_with_spotlight(k[2])
-        end, { desc = k[3] .. " with Spotlight", silent = true })
+      for _, m in ipairs(mappings) do
+        vim.keymap.set({ "n", "x", "o" }, m[1], m[2], {
+          desc = m[3],
+          silent = true,
+        })
       end
     end,
   },
