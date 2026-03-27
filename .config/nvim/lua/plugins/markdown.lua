d63542fd7120d40a2b9b8bab5303ec9295f612aa
Latif <ibootrc@gmail.com>
2026-03-27 18:47:51 +0100

    M  .config/nvim/lua/plugins/markdown.lua

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,54 +1,37 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  ft = { "markdown" },
+  ft = { "markdown", "quarto", "rmd" },
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("render-markdown").setup {
+      preset = "obsidian",
+      heading = {
+        icons = { "   ", "   ", "   ", "   ", "   ", "   " },
+        position = "overlay",
+        -- Disable heading background highlights by setting them to 'none'
+        backgrounds = { "none", "none", "none", "none", "none", "none" },
+      },
+      code = {
+        -- Remove the solid background from code blocks
+        highlight = "Normal",
+        -- If using 'full' width, this prevents the block from painting a background
+        style = "language",
+      },
+    }
 
-  opts = {
-    code = {
-      enabled = true,
-
-      -- Layout only (no theming)
-      width = "block",
-      min_width = 60,
-      left_pad = 2,
-      right_pad = 2,
-
-      -- Keep structure, remove visual styling overrides
-      border = "rounded",
-
-      -- Keep UX features
-      language_icon = true,
-      language_name = true,
-      sign = false,
-    },
-
-    heading = {
-      enabled = true,
-      sign = false,
-      icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-      width = "block",
-    },
-
-    checkbox = {
-      enabled = false,
-    },
-
-    -- Performance safeguards
-    render_modes = true,
-    max_file_size = 200 * 1024,
-  },
-
-  config = function(_, opts)
-    local rm = require "render-markdown"
-    rm.setup(opts)
-
-    -- Auto-disable on large files
-    vim.api.nvim_create_autocmd("BufReadPre", {
-      callback = function(args)
-        local size = vim.fn.getfsize(args.file)
-        if size > 200 * 1024 then
-          rm.set(false)
-        end
-      end,
-    })
+    -- Force-clear the specific background highlights via Neovim API
+    local groups = {
+      "RenderMarkdownH1Bg",
+      "RenderMarkdownH2Bg",
+      "RenderMarkdownH3Bg",
+      "RenderMarkdownH4Bg",
+      "RenderMarkdownH5Bg",
+      "RenderMarkdownH6Bg",
+      "RenderMarkdownCode",
+      "RenderMarkdownCodeInline",
+    }
+    for _, group in ipairs(groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "none", force = true })
+    end
   end,
 }
